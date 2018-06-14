17639a6407d42be20d66f715dcd189aadd36d8e9
Josh Pollock <josh@calderawp.com>
2018-06-14 10:22:27 -0400

    make exports more sensible

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
-"use strict";
+'use strict';
 
-alert(1);
+var _index = require('./fields/index');
+
+var fields = _interopRequireWildcard(_index);
+
+var _index2 = require('./fields/factories/index');
+
+var factories = _interopRequireWildcard(_index2);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+module.exports = {
+	fields: fields,
+	factories: factories
+};
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+import * as fields from './fields/index';
+import * as factories from './fields/factories/index'
+module.exports = {
+	fields,
+	factories
+};
