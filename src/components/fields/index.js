cfc7ca479e36a255e3b9ab2b2af92cd7abbf760d
Josh Pollock <josh@calderawp.com>
2018-06-16 21:00:25 -0400

    lints

--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,8 +1,8 @@
 /*eslint no-undef: "error"*/
 /*eslint-env node*/
-import {FieldGroup} from "./FieldGroup";
-import {SelectField} from "./select/SelectField";
-import {Input} from "./input/Input";
+import {FieldGroup} from './FieldGroup';
+import {SelectField} from './select/SelectField';
+import {Input} from './input/Input';
 
 module.exports = {
 	FieldGroup,
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 /*eslint no-undef: "error"*/
 /*eslint-env node*/
 import * as fields from './fields/index';
-import * as factories from './fields/factories/index'
+import * as factories from './fields/factories/index';
 module.exports = {
 	fields,
 	factories
