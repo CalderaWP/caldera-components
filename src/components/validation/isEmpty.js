200c296b9d2c936b25ddef1628cbafad56d5f20f
Josh Pollock <josh@calderawp.com>
2018-06-30 19:34:19 -0400

    is empty validators

---
+++ src/components/validation/isEmpty.js
@@ -0,0 +1,68 @@
+/**
+ * Validators that check if a value is empty or not
+ */
+export default {
+	/**
+	 * Check if different types (numbers, strings, objects, arrays or Maps) are empty
+	 *
+	 * @param {String|number|Array|object|Map} value
+	 * @return {boolean}
+	 */
+	anything(value){
+		if( Array.isArray(value)){
+			return this.array(value);
+		}
+		if( value instanceof Map ){
+			return this.map(value);
+		}
+		switch (typeof value){
+			case 'object':
+				return this.object(value);
+			case 'string':
+				return this.string(value);
+			default:
+				return false;//
+		}
+		return false;
+	},
+	/**
+	 * Check if value is an empty object
+	 *
+	 * @param {Object} value The object to check
+	 * @return {boolean}
+	 */
+	object(value){
+		for(let key in value) {
+			if(value.hasOwnProperty(key))
+				return false;
+		}
+		return true;
+	},
+	/**
+	 * Check if value is an empty array
+	 *
+	 * @param {Array} value Array to check
+	 * @return {boolean}
+	 */
+	array(value){
+		return 0 === value.length;
+	},
+	/**
+	 * Check if value is not an empty string
+	 *
+	 * @param {String} value String to check
+	 * @return {boolean}
+	 */
+	string(value){
+		return !value;
+	},
+	/**
+	 * Check if value is not an empty map
+	 *
+	 * @param {Map} value The Map to check
+	 * @return {boolean}
+	 */
+	map( value ){
+		return 0 === value.size;
+	}
+}
---
+++ src/components/validation/isEmpty.test.js
@@ -0,0 +1,76 @@
+import isEmpty from './isEmpty';
+describe('Is empty utilities', () => {
+
+	describe( 'methods for data types', () => {
+		it( 'Knows empty objects are empty', () => {
+			expect( isEmpty.object({})).toBe(true);
+		});
+
+		it( 'Knows non-empty objects are not empty', () => {
+			expect( isEmpty.object({roy:'Mike'})).toBe(false);
+		});
+
+		it( 'Knows empty arrays are empty', () => {
+			expect( isEmpty.object([])).toBe(true);
+		});
+
+		it( 'Knows non-empty arrays are not empty', () => {
+			expect( isEmpty.object([{roy:'Mike'},9])).toBe(false);
+		});
+
+		it( 'Knows empty strings are empty', () => {
+			expect( isEmpty.string('')).toBe(true);
+		});
+
+		it( 'Knows non-empty strings are not empty', () => {
+			expect( isEmpty.string('mike')).toBe(false);
+		});
+
+		it( 'Knows empty Maps are  empty', () => {
+			expect( isEmpty.map(new Map())).toBe(true);
+		});
+
+		it( 'Knows non-empty Maps are not empty', () => {
+			const map = new Map().set(1,2);
+			expect( isEmpty.map(map)).toBe(false);
+		});
+	});
+
+	describe( 'anything method for testing all data types', () => {
+		it( 'Knows empty objects are empty', () => {
+			expect( isEmpty.anything({})).toBe(true);
+		});
+
+		it( 'Knows non-empty objects are not empty', () => {
+			expect( isEmpty.anything({roy:'Mike'})).toBe(false);
+		});
+
+		it( 'Knows empty arrays are empty', () => {
+			expect( isEmpty.anything([])).toEqual(true);
+		});
+
+		it( 'Knows non-empty arrays are not empty', () => {
+			expect( isEmpty.anything([7,9])).toEqual(false);
+		});
+
+		it( 'Knows empty strings are empty', () => {
+			expect( isEmpty.anything('')).toBe(true);
+		});
+
+		it( 'Knows non-empty strings are not empty', () => {
+			expect( isEmpty.anything('mike')).toBe(false);
+		});
+
+		it( 'Knows empty Maps are  empty', () => {
+			expect( isEmpty.anything(new Map())).toBe(true);
+		});
+
+		it( 'Knows non-empty Maps are not empty', () => {
+			const map = new Map().set(1,2);
+			expect( isEmpty.anything(map)).toBe(false);
+		});
+	});
+
+
+
+});
